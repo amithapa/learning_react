e8b41d8231c0523b77ad55e4ef516b8dc203278c
Amit Thapa <amit.thapa@niveussolutions.com>
2021-12-15 19:40:51 +0530

    Hooks

--- react-hooks-learn/src/App.js
+++ react-hooks-learn/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
 import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
+import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo />
+      <ClassMouse />
+      {/* <ClassCounterOne /> */}
+      {/* <HookCounterOne /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounterFour /> */}
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
     </div>
