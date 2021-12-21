2ea69c4d708fa681dbdfe02b632d9294f0951a0a
amithapa <amithapa1994@gmail.com>
2021-12-22 01:11:26 +0530

    Callback Hooks

---
+++ callback-hook/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassTimer />
+      <HookTimer  />
+      {/* <FocusInput /> */}
+      {/* <Counter /> */}
+      {/* <ParentComponent /> */}
+    </div>
+  );
+}
+
+export default App;
